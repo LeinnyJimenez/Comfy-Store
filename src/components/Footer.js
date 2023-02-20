3a5d6b6b894b08256b7850804fcc3cdce1dcc899
LeinnyJimenez <jimenexleinny@gmail.com>
2023-02-20 10:44:07 -0400

    sidebar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <Wrapper>
       <h5>
         &copy; {new Date().getFullYear()}
-        <span>comfySloth</span>
+        <span> comfySloth</span>
       </h5>
       <h5>All rights reserved</h5>
     </Wrapper>
