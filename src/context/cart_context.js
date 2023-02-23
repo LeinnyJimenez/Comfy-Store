f11c90a6b69b46771b96c77e094c6fa0fca8e26f
LeinnyJimenez <jimenexleinny@gmail.com>
2023-02-22 21:13:46 -0400

    cart page first setup

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -22,11 +22,24 @@ export const CartProvider = ({ children }) => {
   // Add to cart 
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
+
   }
+
+  // remove item
+  const removeItem = (id) => {
+
+  }
+
+  // toggle amount 
+  const toggleAmount = (id, value) => { }
+
+  // clear cart
+  const clearCart = () => { }
+
 
   const [state, dispatch] = useReducer(reducer, initialState)
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}> {children}</CartContext.Provider >
+    <CartContext.Provider value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}> {children}</CartContext.Provider >
   )
 }
 // make sure use
